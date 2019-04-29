8ab3f042bf7b51c7251483d77d5784d0d820f874
Piotr Mikulski <piotr@imo.im>
2019-04-29 14:46:52 -0700

    make run_it.sh work in any folder

--- run_it.zsh
+++ run_it.zsh
@@ -1,3 +1,8 @@
 #!/usr/bin/env zsh
+
+
+export SCRIPT_DIR="$(dirname $(readlink -f $0))"
+
+cd $SCRIPT_DIR
 java -cp `echo *jar | sed 's/ /:/g'` com.topcoder.client.contestApplet.runner.generic  www.topcoder.com 5001 "http://tunnel1.topcoder.com/tunnel1?dummy" TopCoder
 
